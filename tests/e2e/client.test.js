412905d1a4c9bc22bcca56bc74e1b5431a939850
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2024-02-10 14:37:20 +0500

    fix: test cases

--- tests/e2e/client.test.js
+++ tests/e2e/client.test.js
@@ -8,8 +8,8 @@ const outputObj = {
   DEV_DB_USERNAME: "root_value.admin",
   DEV_DB_PASSWORD: "root_value.admin.5000.12345",
   DEV_SECURE: false,
-  PROD_HOST: "app.cool.com",
   PROD_PORT: 8080,
+  PROD_HOST: "app.cool.com",
   PROD_DB_USERNAME: "admin",
   PROD_DB_PASSWORD: "136_@rongPas.",
   PROD_SECURE: true,
@@ -17,9 +17,11 @@ const outputObj = {
   LANG: "fr/FR",
 }
 
+const benvFile = __dirname + "/syntax/nesting.benv"
+
 describe("Client", () => {
   it("should load benv from file", () => {
-    const env = new BetterDotEnv().load("./syntax/nesting.benv").env
+    const env = new BetterDotEnv().load(benvFile).env
     expect(env).toEqual(outputObj)
   })
 
@@ -28,13 +30,13 @@ describe("Client", () => {
       EXISITING_VAR1: "HELLO",
       EXISITING_VAR2: "HELLO 2",
     }
-    new BetterDotEnv().load("./syntax/nesting.benv").append(env)
+    new BetterDotEnv().load(benvFile).append(env)
     expect(env).toEqual({ ...env, ...outputObj })
   })
 
   it("should emit .env file", async () => {
     const filePath = "./prod.env"
-    new BetterDotEnv().load("./syntax/nesting.benv").emit(filePath)
+    new BetterDotEnv().load(benvFile).emit(filePath)
 
     const env = await readFile(filePath)
     const envText = Object.entries(outputObj)
@@ -47,7 +49,7 @@ describe("Client", () => {
   })
 
   it("should not allow to edit env object", () => {
-    const env = new BetterDotEnv().load("./syntax/nesting.benv").env
+    const env = new BetterDotEnv().load(benvFile).env
     env.ADDED_VAR = "cool"
     env.DEV_DB_USERNAME = "new username"
     delete env.DEV_DB_PASSWORD
--- tests/e2e/nesting.test.js
+++ tests/e2e/nesting.test.js
@@ -2,40 +2,43 @@ const { executeCommand, readFile, deleteFile, objectToEnv } = require("../_helpe
 const { BetterDotEnv } = require("../../dist/client")
 
 const outputObj = {
-  DEV_DB_PASSWORD: "root_value.admin.5000.12345",
-  DEV_DB_USERNAME: "root_value.admin",
-  DEV_HOST: "https://localhost:5000",
+  ROOT_VAR: "root_value",
   DEV_PORT: 5000,
+  DEV_HOST: "https://localhost:5000",
+  DEV_DB_USERNAME: "root_value.admin",
+  DEV_DB_PASSWORD: "root_value.admin.5000.12345",
   DEV_SECURE: false,
+  PROD_PORT: 8080,
+  PROD_HOST: "app.cool.com",
+  PROD_DB_USERNAME: "admin",
+  PROD_DB_PASSWORD: "136_@rongPas.",
+  PROD_SECURE: true,
   ENV: "dev",
   LANG: "fr/FR",
-  PROD_DB_PASSWORD: "136_@rongPas.",
-  PROD_DB_USERNAME: "admin",
-  PROD_HOST: "app.cool.com",
-  PROD_PORT: 8080,
-  PROD_SECURE: true,
-  ROOT_VAR: "root_value",
 }
 
 const outputEnv = objectToEnv(outputObj)
+
+const benvFile = __dirname + "/syntax/nesting.benv"
 
 describe("Indetation", () => {
   describe("Client", () => {
     it("should identify indented blocks", () => {
       const env = {}
-      new BetterDotEnv().load("./syntax/nesting.benv").append(env)
+      new BetterDotEnv().load(benvFile).append(env)
       expect(env).toEqual(outputObj)
     })
   })
   describe("CLI", () => {
+    const envFile = __dirname + "/../../nesting.env"
     it("should identify indented blocks", async () => {
-      const out = await executeCommand("node ./dist/main.js ./syntax/nesting.benv")
+      const out = await executeCommand("node ./dist/main.js " + __dirname + "/syntax/nesting.benv")
       expect(out.trim()).toEqual("Generated: nesting.env")
 
-      const env = await readFile("./nesting.env")
+      const env = await readFile(envFile)
       expect(env).toEqual(outputEnv)
 
-      await deleteFile("./nesting.env")
+      await deleteFile(envFile)
     })
   })
 })
